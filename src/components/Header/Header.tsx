289f291ea17902c2bae02c448d6665191d066311
Zayac11 <brunogrbstudios@gmail.com>
2022-10-14 22:38:06 +0300

    Added Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Header.module.scss'
+import {ReactComponent as Logo} from '../../assets/logo.svg'
+
+const Header = () => {
+    return (
+        <div className={styles['container']}>
+            <div className={styles['logo']}>
+                <Logo />
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Dashboard.module.scss'
 import Video from '../../components/Video/Video'
+import Header from '../../components/Header/Header'
 
 export const Dashboard = () => {
     return (
         <div>
-            <Video />
+            <Header />
         </div>
     )
 }
